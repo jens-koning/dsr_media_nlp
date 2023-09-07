8171ad42066d5813577444ea2ccdc7dd2a23c58e
Jens Koning <jens.jjk@gmail.com>
2023-09-07 14:45:05 +0200

    v0.4

---
+++ dsr_media_nlp/Script/country_specific_params.py
@@ -0,0 +1,23 @@
+####        \\      Country specific searches        //       ####
+
+params = [
+    {
+    'q': 'Digital Silk Road AND DSR',
+    'lang': 'en',
+    'countries': 'MY,PH,ID',
+    'to_rank': 10000,
+    'page_size': 100,
+    'from': '3 years ago', 
+    'page': 1
+    },
+    {
+    'q': 'Digital Silk Road AND China',
+    'lang': 'en',
+    'countries': 'MY,PH,ID',
+    'to_rank': 10000,
+    'page_size': 100,
+    'from': '3 years ago', 
+    'page': 1
+    }
+]
+
