0b10a0bd051a3d92fe6dc86df6094a775c5cd926
Jens Koning <jens.jjk@gmail.com>
2023-09-14 13:31:01 +0200

    v0.52

---
+++ dsr_media_nlp/Topic_model/Seed_topic_list.py
@@ -0,0 +1,8 @@
+#                       seed_topic_list=[
+        ["huawei", "dsr", "smart", "city", "5g"],
+        ["infrastructure", "b3w", "g7", "bri"],
+        ["aseab", "rcep", "ina"],
+        ["military", "cooperation", "ningxia"],
+        ["strategic", "infrastructure"]
+    ]
+                       )
